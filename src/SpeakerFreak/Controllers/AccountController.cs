d85669b07c2510830a63276df6a89f4ffabde4e9
rouz majlessi <rmajlessi@gmail.com>
2016-12-27 00:18:54 -0800

    add Create new user to accountcontroller with email and password

--- src/SpeakerFreak/Controllers/AccountController.cs
+++ src/SpeakerFreak/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SpeakerFreak.Models;
 using Microsoft.AspNetCore.Identity;
+using SpeakerFreak.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,7 +16,7 @@ namespace SpeakerFreak.Controllers
         private readonly ApplicationDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
+    
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager,ApplicationDbContext db)
         {
 
@@ -29,5 +30,22 @@ namespace SpeakerFreak.Controllers
         {
             return View();
         }
+
+        //Creat Register View
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            // instantiate a new user , via through applicationUser, which passes our viewModel email. then _userManager (new user) can create password which is also passed through viewModel.
+            var user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
