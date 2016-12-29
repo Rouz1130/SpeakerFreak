f71fc9154c10a598c7b55d95b1919ed857a43480
rouz majlessi <rmajlessi@gmail.com>
2016-12-28 19:43:34 -0800

    add model to database did not work last update

--- src/SpeakerFreak/Models/ApplicationDbContext.cs
+++ src/SpeakerFreak/Models/ApplicationDbContext.cs
@@ -9,14 +9,27 @@ namespace SpeakerFreak.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
+        public ApplicationDbContext()
         {
             //need to have this open constructor part to not have error 
         }
+        //might add virtual if i make another table
+        public DbSet<Music> Muscians { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpeakerFreak;integrated security=True");
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
-        public DbSet<Music> Musicians { get; set; }
     }
 }
+
