ce699e228173a52c61428a0ccdbff4aeedfd35b0
rouz majlessi <rmajlessi@gmail.com>
2016-12-28 20:03:08 -0800

    finally update music table with migrations ignore two previous commit

--- src/SpeakerFreak/Migrations/20161229035811_AddMusicTable.Designer.cs
+++ src/SpeakerFreak/Migrations/20161229035811_AddMusicTable.Designer.cs
@@ -8,9 +8,10 @@ using SpeakerFreak.Models;
 namespace SpeakerFreak.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161229035811_AddMusicTable")]
+    partial class AddMusicTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
--- src/SpeakerFreak/Models/ApplicationDbContext.cs
+++ src/SpeakerFreak/Models/ApplicationDbContext.cs
@@ -9,27 +9,18 @@ namespace SpeakerFreak.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions options) : base(options)
         {
             //need to have this open constructor part to not have error 
         }
-        //might add virtual if i make another table
-        public DbSet<Music> Muscians { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-        {
-            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SpeakerFreak;integrated security=True");
-        }
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
         }
+
+
+        public DbSet<Music> Muscian { get; set; }
     }
 }
-
